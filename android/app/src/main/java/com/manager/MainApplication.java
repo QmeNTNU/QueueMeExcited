a45bd0058b80d5cb99dd98f117da5b29c1fdce5e
andykamp <akampenes@gmail.com>
2017-07-23 22:27:45 +0200

    Revert "Betatest for android"

--- android/app/src/main/java/com/manager/MainApplication.java
+++ android/app/src/main/java/com/manager/MainApplication.java
@@ -1,4 +1,4 @@
-package com.QueueMeExcited;
+package com.manager;
 
 import android.app.Application;
 
@@ -7,9 +7,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,7 +35,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
